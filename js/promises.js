9b9f9f1877bc67471a7e24a679990d92b0ea00b9
Michal <michalgrzegorczyk04@gmail.com>
2021-09-16 14:04:57 +0200

    Add async await and async await fetch

--- js/promises.js
+++ js/promises.js
@@ -14,7 +14,7 @@
         }, 1000);
     }
     
-    function createposts(post) {
+    function createPost(post) {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 posts.push(post);
@@ -29,8 +29,42 @@
             }, 2000);
         });
     }
+
+    // PROMISE ALL START
+        // const promise1 = Promise.resolve('Hello World!');
+        // const promise2 = 10;
+        // const promise3 = new Promise((resolve, reject) => {
+        //     setTimeout(resolve, 2000, 'Goodbye');
+        // });
+        // const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then(res => res.json());
+
+        // Promise.all([promise1, promise2, promise3, promise4])
+        //     .then(values => {
+        //         console.log(values);
+        //     });
     
-    createposts({ title: 'Post Three', body: 'This is a body from post Three' })
-        .then(getPosts)
-        .catch(err => console.log(err)); // reject if error is true   
+
+    // PROMISE
+        createPost({ title: 'Post Three', body: 'This is a body from post Three' })
+            .then(getPosts)
+            .catch(err => console.log(err)); // reject if error is true   
+
+
+    // ASYNC / AWAIT START
+        // async function init() {
+        //     await createPost({ title: 'Post Three', body: 'This is a body from post Three' });
+        //     getPosts();
+        // }
+        // init();
+
+
+    // ASYNC / AWAIT / FETCH 
+        // async function fetchUsers() {
+        //     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+        //     const data = await res.json();
+        //     console.log(data);
+        // }
+        // fetchUsers();
 })();

5b9563ed35690ac2f4d16a193d4aadd6cc61ede5
Michal <michalgrzegorczyk04@gmail.com>
2021-09-16 13:58:23 +0200

    Add promise all

--- js/callbacks.js
+++ js/callbacks.js
@@ -14,13 +14,33 @@
         }, 1000);
     }
 
-    function createPosts(post, callback) {
-        setTimeout(() => {
-            posts.push(post);
-            callback();
-        }, 2000);
-    }
+    // function createPosts(post, callback) {
+    //     setTimeout(() => {
+    //         posts.push(post);
+    //         callback();
+    //     }, 2000);
+    // }
 
-    getPosts();
-    createPosts({ title: 'Post Three', body: 'This is a body from post Three' }, getPosts);
+    // getPosts();
+    // createPosts({ title: 'Post Three', body: 'This is a body from post Three' }, getPosts);
+
+    // PROMISE ALL START
+        // const promise1 = Promise.resolve('Hello World!');
+        // const promise2 = 10;
+        // const promise3 = new Promise((resolve, reject) => {
+        //     setTimeout(resolve, 2000, 'Goodbye');
+        // });
+        // const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then(res => res.json());
+
+        // Promise.all([promise1, promise2, promise3, promise4])
+        //     .then(values => {
+        //         console.log(values);
+        //     });
+
+    // ASYNC / AWAIT START
+        async function init() {
+            
+        }
+
 })();
